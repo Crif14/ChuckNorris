535d25aa7d147755b35f56abc2fefa064cfc3b01
Crif14 <csqueo22@gmail.com>
2025-02-23 21:36:11 +0100

    lavoro finito

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,28 @@
-fetch("https://api.chucknorris.io/jokes/random?category={category}")
+let buttons = document.querySelectorAll(".dropdown-item");
+let categoria = "";
+let button = document.querySelector("#caricaJoke");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (event) => {
+        let el = event.currentTarget;
+        categoria = el.textContent.trim();
+        document.querySelector("#battutaDropdown").textContent = "Categoria: " + categoria;
+        button.classList.remove("buttonError")
+    });
+}
+
+button.addEventListener('click', function () {
+    if (categoria === "") {
+        button.classList.add("buttonError");
+        return;
+    }
+    fetch("https://api.chucknorris.io/jokes/random?category=" + categoria.toLowerCase())
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector("#battuta").innerHTML = data.value;
+            document.querySelector("#imgChuck").src = "static/image/chuck_parla.png";
+            document.querySelector("#imgNuvola").classList.remove("hide");
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
